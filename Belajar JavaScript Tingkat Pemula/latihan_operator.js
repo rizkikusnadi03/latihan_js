4f388f53b6faa98079e1f0e9d0621ede3ecfbaf7
rizkikusnadi03 <rizkikusnadi03@gmail.com>
2026-01-12 02:47:28 +0700

    latihan operator

---
+++ Belajar JavaScript Tingkat Pemula/latihan_operator.js
@@ -0,0 +1,33 @@
+// Menghitung volume kubus
+const balok = 30;
+let volumekubus = balok ** 3;
+console.log(volumekubus);
+
+// Menghitung luas lingkaran
+const phi = 3.14;
+let jariJari = 7;
+let luasLingkaran = phi * jariJari ** 2;
+console.log(luasLingkaran);
+
+// Menghitung sisa pembagian
+let angkat1 = 32;
+let angkat2 = 10;
+let sisapembagian = angkat1 % angkat2;
+console.log(sisapembagian);
+
+// Operator penggabungan string
+let kata1 = "hai, selamat ";
+let kata2 = "datang di bahasa pemerogramman js";
+let gabungkanKata = kata1 + kata2;
+console.log(gabungkanKata);
+
+// Operator penggabungan string dengan penugasan
+let salam = "halo ";
+salam += "apa kabar?";
+console.log(salam);
+
+// operator perbandingan dengan tipe data string
+let nama1 = "kustomo";
+let nama2 = "kustomo";
+let bandingkanNama = nama1 == nama2;
+console.log(bandingkanNama);
