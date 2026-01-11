8f9c28298207c28e375af342f95221b8d4636740
rizkikusnadi03 <rizkikusnadi03@gmail.com>
2026-01-11 22:37:55 +0700

    variable

---
+++ Belajar JavaScript Tingkat Pemula/variabel.js
@@ -0,0 +1,14 @@
+// buat variabel 
+// Number
+let angka = 10;
+console.log(angka);
+// String
+let nama = "kustomo";
+console.log(nama);
+
+const isCodingFun = true;
+console.log(isCodingFun);
+
+// Boolean
+let isRaining = false;
+console.log(isRaining);
