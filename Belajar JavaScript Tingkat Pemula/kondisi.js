02486d0da687c21bd0e8e8565d4c3fe17f89a3f3
rizkikusnadi03 <rizkikusnadi03@gmail.com>
2026-01-12 13:24:15 +0700

    operator bolean

--- Belajar JavaScript Tingkat Pemula/kondisi.js
+++ Belajar JavaScript Tingkat Pemula/kondisi.js
@@ -1,1 +1,25 @@
 // if, if else, if else if, switch
+let nilai = 85;
+
+// if
+if (nilai >= 80) {
+    console.log("Selamat, Anda lulus!");
+}
+// if else
+if (nilai >= 86) {
+    console.log("Selamat, Anda lulus!");
+} else {
+    console.log
+        ("Maaf, Anda tidak lulus.");
+}
+
+// operattor boolean
+let absen = 80;
+let tugas = 85;
+let nilaiAkhir = (nilai + absen + tugas) / 3;
+
+if (nilai >= 70 && absen >= 75 && tugas >= 80 && nilaiAkhir >= 75) {
+    console.log("Selamat, Anda lulus Mata Pelajaran!");
+} else {
+    console.log("Maaf, Anda tidak lulus.");
+}
